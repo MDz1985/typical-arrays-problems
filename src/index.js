4d57a4b07f0941c6334bd9d7b00cf34b0d8d3d2f
MDz1985 <m_d@tut.by>
2021-08-13 12:20:48 +0300

    feat: all done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
 
 exports.min = function min (array) {
-  return 0;
+  let min = 1000000;
+
+  if (array === undefined) {
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  for (let i = 0 ; i < array.length; i++){
+    if (min > array[i]){
+      min = array[i];
+    }
+  }
+  return min;
+
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max =-100000;
+  if (array === undefined) {
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  for (let i = 0 ; i < array.length; i++){
+    if (max < array[i]){
+      max = array[i];
+    }
+  }
+  return max;
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum=0;
+  if (array === undefined) {
+    return 0;
+  }
+  if (array.length === 0) {
+    return 0;
+  }
+  for (let i = 0 ; i < array.length; i++){
+    
+      sum += array[i];
+  }
+  
+  return sum/array.length;
+
 }
+
